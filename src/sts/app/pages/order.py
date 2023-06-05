6a88bd9588cc375e0f4e6a4c85cff3e80d5a7683
Moritz Enderle <moritz.enderle@stud.th-deg.de>
2023-06-05 08:49:51 +0200

    feat: Improved page navigation

--- src/sts/app/pages/order.py
+++ src/sts/app/pages/order.py
@@ -14,62 +14,106 @@ from sts.utils.utils import Cart, transfer
 # Function to upload and display image
 
 def upload_image(column_num):
-    uploaded_file = st.sidebar.file_uploader(f"Upload Image {column_num}", type=["png", "jpg", "jpeg"])
-    
+    uploaded_file = st.file_uploader(f"Upload Image {column_num}", type=["png", "jpg", "jpeg"], label_visibility="hidden")
+
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
-        st.image(image, caption=f"Uploaded Image {column_num}", use_column_width=True)
-        return image
+        st.session_state["images"][column_num - 1] = image
+
+    if st.session_state["images"][column_num - 1] is not None:
+        st.image(st.session_state["images"][column_num - 1], caption=f"Uploaded Image {column_num}", use_column_width=True)
+    
+
+def create_image():
+
+    st.title("Create AI Image")
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.subheader("Image 1")
+        upload_image(1)
+
+    with col2:
+        st.subheader("Image 2")
+        upload_image(2)
+
+    if all([image is not None for image in st.session_state["images"]]):
+        st.markdown("## AI Image")
+        ai_image = transfer(st.session_state["images"][0], st.session_state["images"][1])
+        st.image(ai_image, caption="AI Image", use_column_width=True)
+
+
+def cart():
+    # TODO Josh: Implement this function
+    st.title("Cart")
+    pass
+
+
+def place_product():
+    # TODO Josh: Implement this function
+    st.title("Place Product")
+    pass
+
+
+def checkout():
+    # TODO Josh: Implement this function
+    st.title("Checkout")
+    st.session_state["current_page"] = "checkout"
+    # TODO Josh: Move this into a function in utils
+    auth = get_authenticator()
+    res = auth.login("Login to access the app", location="sidebar")
+
+    if not res[1]:
+        st.markdown("# Stop")
+        st.markdown("Please login to use this feature")
+        st.stop()
+    # THIS REQUIRES AUTHENTICATION
+
+    pass
+
+
+def home():
+    st.title("Home")
+    st.markdown("Welcome to the Style Transfer Shop!")
 
 
 def main() -> None:
     # add sidebar with create_image, place product, cart, checkout
     st.sidebar.title("Order Process")
 
-    if st.sidebar.button("Cart [0]", key="cart_button", use_container_width=True):
-        st.title("Cart")
+    if "images" not in st.session_state:
+        st.session_state["images"] = [None, None]
 
-    # Two columns for uploading and displaying images
-    col1, col2 = st.columns(2)
+    if "ai_image" not in st.session_state:
+        st.session_state["ai_image"] = None
 
-    with col1:
-        st.subheader("Image 1")
-        image1 = upload_image(1)
+    if "current_page" not in st.session_state:
+        st.session_state["current_page"] = home
 
-    with col2:
-        st.subheader("Image 2")
-        image2 = upload_image(2)
+    cart_btn = st.sidebar.button("Cart [0]", key="cart_button", use_container_width=True)
+    create_image_btn = st.sidebar.button("Create AI Image", key="create_image_button", use_container_width=True)
+    place_product_btn = st.sidebar.button("Place Product", use_container_width=True, disabled=True)
+    checkout_btn = st.sidebar.button("Checkout", use_container_width=True, disabled=True)
 
-    if st.sidebar.button("Create AI Image", key="create_image_button", use_container_width=True):
-        if image1 is None or image2 is None:
-            st.warning("Please upload both images before creating AI image.")
-        else:
-            st.title("AI Image Creation")
-            # Apply transfer style and display the result image
-            ai_image = transfer(image1, image2)
-            st.image(ai_image, caption="AI Image", use_column_width=True)
+    if cart_btn:
+        cart()
+        st.session_state["current_page"] = cart
 
+    elif create_image_btn:
+        create_image()
+        st.session_state["current_page"] = create_image
 
-    # only enable if image is created
-    if st.sidebar.button("Place Product", use_container_width=True, disabled=True):
-        # DO STUFF
-        st.title("Place Product")
+    elif place_product_btn:
+        place_product()
+        st.session_state["current_page"] = place_product
 
-    # only enable if cart is not empty
-    if st.sidebar.button("Checkout", use_container_width=True, disabled=True):
-        # TODO Josh: Move this into a function in utils
-        auth = get_authenticator()
-        res = auth.login("Login to access the app", location="sidebar")
+    elif checkout_btn:
+        checkout()
+        st.session_state["current_page"] = checkout
 
-        if not res[1]:
-            st.markdown("# Stop")
-            st.markdown("Please login to use this feature")
-            st.stop()
-        # THIS REQUIRES AUTHENTICATION
-
-        # DO STUFF
-        st.title("Checkout")
-
+    else:
+        st.session_state["current_page"]()
 
 if __name__ == "__main__":
     main()
